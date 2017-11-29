8e00692547f18b0eb6f9170544b0660d2e0ca038
Radionovdon <radionovdon@gmail.com>
2017-11-29 14:42:16 +0200

    +arrow up

--- src/js/script.js
+++ src/js/script.js
@@ -159,4 +159,27 @@ let sharing = $('.footer-social-item:nth-child(2)'),
 		socialBoxSharing.slideToggle('fast');
 	});
 
+
+	$('.article-content-up-link').on('click', function (e) {
+		e.preventDefault();
+		let id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+
+		let artHght = 500;
+
+	$(window).scroll(function(){
+
+		let top = $(this).scrollTop();
+		let upLink = $('.article-content-up-link');
+
+		if (top > artHght) {
+			upLink.css('display', 'block');
+		} else {
+			upLink.css('display', 'none');
+		}
+
+	});
+
 });
